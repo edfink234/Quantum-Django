909f9edc8fffbd41f32b4ddd382d947beb633b0e
edfink234 <edfink234@gmail.com>
2023-06-22 15:33:24 +0200

    Updating views.py for new users

--- myworld/members/views.py
+++ myworld/members/views.py
@@ -65,52 +65,57 @@ def index(request): #Corresponds to myfirst.html
     if request.user.is_authenticated:
         username = request.user.username #setting the username
     print(username)
-    html_string = ""
-    user_db = db.posts.find_one({"user": username})
-    if user_db:
-        #getting the data of the user if existing already
-        html_string = db.posts.find_one({"user": username}).get('index_data') #returns None if index not found in dict
-        activated_channels = db.posts.find_one({"user": username}).get('activatedChannels') #returns None if index not found in dict
-        
-        functionDict = {'729Spec': {'params' : {'startFreq': {'type': 'float', 'default': 10.0, 'info': 'Defines the start frequency of the measurement.'},
-                 'stopFreq': {'type': 'float', 'default': 20.0, 'info': 'Defines the stop fequency of the measurement.'},
-                 'stepSize': {'type': 'float', 'default': 2, 'info': 'Defines the step size between two measurement points.'},
-                 'quenchAfter': {'type': 'bool', 'default': True, 'info': 'If True, quenches after measurement is completed.'}
-                },
-                'description' : 'This Experiment does 729 Spectroscopy.',
-                'other_metadata' : []
-                } ,
-                '729ShelvingTest' : {'params' : { 'Location': {'type': 'str', 'default': '', 'info': 'Location of Shelving Test.'}
-                      },
-                   'description' : 'This Experiment does a 729 Shelving Test.',
-                'other_metadata' : []
-                } ,
-                'RamanCalibrationScan' : {'params' : { 'piTime': {'type': 'float', 'default': 1.0e-5, 'info': 'Pi pulse duration.'},
-                        'repetitions': {'type': 'int', 'default': 1, 'info': 'Amount of times the Scan is repeated.'},
-                    },
-                'description' : 'This Experiment does a Raman Calibration Scan.',
-                   'other_metadata' : []
-                   },
-                'MeasureBeamProfile' : {'params' : {} ,
-                'description' : 'This Experiment measures the Beam Profile.',
-                 'other_metadata' : []
-                }
-            }
-        
-        if user_db.get("functionDict") == None:
-            db.posts.find_one_and_update({"user": username}, { '$set': {"functionDict": functionDict}})
-            user_db = db.posts.find_one({"user": username})
-            
-        print(user_db.get("functionDict"))
-        
-#        print(activated_channels, type(activated_channels[0]))
+    activated_channels = []
     
-    #First time: html_string is None
-    if not html_string:
-        html_string = ""
-    print("html_string =",html_string)
+    user_db = db.posts.find_one({"user": username})
+    
+#    Uncomment the following two lines to delete the current user and test it again
+#    db.posts.find_one_and_delete({"user": username})
+#    user_db = db.posts.find_one({"user": username})
+    
+    print("user_db =",user_db)
+    if user_db == None:
+        db.posts.insert_one({"user": username})
+        user_db = db.posts.find_one({"user": username})
+    #getting the data of the user if existing already
+    if user_db.get('index_data') == None:
+        db.posts.find_one_and_update({"user": username}, { '$set': {"index_data": ""}})
+
+    if user_db.get('activatedChannels') == None:
+        db.posts.find_one_and_update({"user": username}, { '$set': {"activatedChannels": [True]*16}})
+
+    functionDict = {'729Spec': {'params' : {'startFreq': {'type': 'float', 'default': 10.0, 'info': 'Defines the start frequency of the measurement.'},
+             'stopFreq': {'type': 'float', 'default': 20.0, 'info': 'Defines the stop fequency of the measurement.'},
+             'stepSize': {'type': 'float', 'default': 2, 'info': 'Defines the step size between two measurement points.'},
+             'quenchAfter': {'type': 'bool', 'default': True, 'info': 'If True, quenches after measurement is completed.'}
+            },
+            'description' : 'This Experiment does 729 Spectroscopy.',
+            'other_metadata' : []
+            } ,
+            '729ShelvingTest' : {'params' : { 'Location': {'type': 'str', 'default': '', 'info': 'Location of Shelving Test.'}
+                  },
+               'description' : 'This Experiment does a 729 Shelving Test.',
+            'other_metadata' : []
+            } ,
+            'RamanCalibrationScan' : {'params' : { 'piTime': {'type': 'float', 'default': 1.0e-5, 'info': 'Pi pulse duration.'},
+                    'repetitions': {'type': 'int', 'default': 1, 'info': 'Amount of times the Scan is repeated.'},
+                },
+            'description' : 'This Experiment does a Raman Calibration Scan.',
+               'other_metadata' : []
+               },
+            'MeasureBeamProfile' : {'params' : {} ,
+            'description' : 'This Experiment measures the Beam Profile.',
+             'other_metadata' : []
+            }
+        }
+    
+    if user_db.get("functionDict") == None:
+        db.posts.find_one_and_update({"user": username}, { '$set': {"functionDict": functionDict}})
+    print("hi")
+    user_db = db.posts.find_one({"user": username})
+
     #return the users html (stored in html_string) as a django variable that can be rendered at the bottom of Raman.html
-    return render(request, r"myfirst.html", context = {'gui_elements' : html_string, 'channelsActivated' : activated_channels, 'functionDict' : json.dumps(user_db.get("functionDict"))})
+    return render(request, r"myfirst.html", context = {'gui_elements' : user_db.get('index_data'), 'channelsActivated' : user_db.get('activatedChannels'), 'functionDict' : json.dumps(user_db.get("functionDict"))})
 
 def Detection(request):
     return render(request, r"Detection.html")
